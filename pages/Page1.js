04754f0a8263f17ac55efae52678f1e25e1b1b12
gajen007@gmail.com <gajen007@gmail.com>
2023-04-18 13:07:01 -0400

    without persist

--- pages/Page1.js
+++ pages/Page1.js
@@ -6,10 +6,11 @@ export default function Page1() {
     const [inputValue,setInputVal] = useState("");
     const saveInput=()=>{
         dispatchWhenClicked(saveToStore(inputValue));
+        alert("Data Stored in Redux-Store! Now check the Page 2!");
     };
 return <div>
         <input type="text" onChange={(e)=>setInputVal(e.target.value)}/>
         <br/>
-        <button onClick={()=>saveInput()}>Save Locally</button>
+        <button onClick={()=>saveInput()}>Save to Redux Store</button>
     </div>;
 }
