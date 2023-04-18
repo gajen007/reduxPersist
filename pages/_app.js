209976f6ec0e30024b21fe9203a666fdbebd4d14
gajen007@gmail.com <gajen007@gmail.com>
2023-04-18 15:09:58 -0400

    with Redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import { Provider } from "react-redux";
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import store from "../redux/store";
+let persistor = persistStore(store);
+
 export default function App({ Component, pageProps }) {
-  return <Provider store={store}><Component {...pageProps} /></Provider>
-}    
+  return <Provider store={store}>
+          <PersistGate persistor={persistor}> 
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+}
--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,33 @@
-import { configureStore } from "@reduxjs/toolkit";
+
+// export default configureStore({
+// 	reducer: { forTest: testReducer }
+// });
+import {configureStore} from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer,
+	FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,} from 'redux-persist';
 import testReducer from "./myreducer";
 
+const persistConfig = {
+    key: 'root',
+    storage:storage
+};
+
+const reducers = combineReducers({ forTest: testReducer });
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export default configureStore({
-	reducer: {
-		forTest: testReducer
-	}
+    reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+	getDefaultMiddleware({
+		serializableCheck: {
+			ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+		},
+	}),
 });
